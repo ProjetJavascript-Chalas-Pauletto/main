cf0403edf381b8b5b245536f1f048aa72717b35f
Chalas Paul <paul-romain.chalas@etu.univ-amu.fr>
2018-03-02 18:13:19 +0100

    Adds

--- js/main.js
+++ js/main.js
@@ -5,11 +5,54 @@
         "Une erreur critique vient de se produire," + //Spreading mail adress in order to provide bots from getting it.
         "veuillez contacter l'administrateur à cette adresse mail : ";
 
+    let css_blanc = {
+        'background-color': 'black',
+        'font-size': 'bolder',
+        'border': 'solid 1px white',
+        'color': 'white',
+        'height': '30px',
+        'width': '30px',
+        'text-align': 'center'
+    };
+    let css_noir = {
+        'background-color': 'yellow',
+        'font-size': 'bolder',
+        'border': 'solid 1px black',
+        'color': 'black',
+        'height': '30px',
+        'width': '30px',
+        'text-align': 'center'
+    };
+    let css_enhanced = {
+        'background-color': 'red',
+        'font-size': 'bolder',
+        'border': 'solid 1px black',
+        'color': 'white',
+        'height': '30px',
+        'width': '30px',
+        'text-align': 'center'
+    };
+
+
     $(document).ready(function() {
 
         setTimeout(function(){
             $('body').addClass('loaded');
         }, 6600);
+
+
+        new Grid(10,10, '#gridTest');
+        $('.case-blanche').css(css_blanc).hover(function () {
+            $(this).css(css_enhanced);
+        }, function () {
+            $(this).css(css_blanc);
+        });
+
+        $('.case-noire').css(css_noir).hover(function () {
+            $(this).css(css_enhanced);
+        }, function () {
+            $(this).css(css_noir);
+        });
 
     });
 
@@ -17,13 +60,14 @@
         $.ajax({
             url: '../json/isLogged.php'
         }).done(function (data) {
-            if (data.result) {
+            if (data.result) { // User connected
                 $('#logout-form').show();
                 $('#bt-menu').show();
                 $('#job').show();
 
-            } else {
+            } else { // User not connected
                 $('#login-form').show();
+                $('#loader-wrapper').remove(); // Delete loading animation
             }
         }).fail(function () {
             $("body").html(erreurCritique);
